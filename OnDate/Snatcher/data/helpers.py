34a8e3c8b1dbfe609b1402062bdacf4702cc2d47
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-08-16 16:29:41 +0300

    project init

---
+++ OnDate/Snatcher/data/helpers.py
@@ -0,0 +1,81 @@
+import os
+import json
+
+from pathlib import Path
+
+from .values import op
+
+
+# ===================================< SWITCH
+# dic - dictionary to use
+# key - from the dictionary to flip
+def switch(dic, key):
+    dic[key] = 1 - dic[key]
+
+
+# ===================================< FILES COUNT
+def files_count(file_path):
+    return sum(len(files) for _, _, files in os.walk(file_path))
+
+
+# ===================================< GET DIRECTORY
+def getDir():
+    return str(Path(__file__).resolve().parent.parent)
+
+
+# ===================================< WRITE FILE
+def write_file(file_path, file_name, data):
+    if not isinstance(file_path, str) or not os.path.exists(file_path):
+        return None
+
+    with open(f'{file_path}/{file_name}', 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+# ===================================< READ FILE
+# file_path - to read from
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        write_file(getDir(), 'config.json', op)
+
+        return None
+
+
+# ===================================< PATH MTIME
+# folder_path - to check mtime for
+def dir_mtime(folder_path):
+    newest = 0
+
+    for root, dirs, files in os.walk(folder_path):
+        try:
+            newest = max(newest, os.path.getmtime(root))
+        except OSError:
+            pass;
+
+        for file in files:
+            path = os.path.join(root, file)
+            try:
+                mtime = os.path.getmtime(path)
+                if mtime > newest:
+                    newest = mtime
+            except OSError:
+                pass
+
+    return newest
+
+
+# ===================================< CONFIG PARSER
+# string - to pars in to config
+def config_parse(string):
+    if string == '' or string is None:
+        return
+
+    config_pack = json.loads(string)
+
+    for name, value in config_pack.items():
+        for key, val in value.items():
+            if key != 'name':
+                op[name][key] = val
